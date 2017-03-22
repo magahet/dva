d8ff7544a624809a725862f39c403c9139b972ae
Gar Mendiola <gmendiola@connexity.com>
2017-03-22 23:09:35 +1100

    hw3 q4 done

--- hw/hw3/hw3.R
+++ hw/hw3/hw3.R
@@ -12,6 +12,7 @@ test <- as.matrix(read.csv('mnist_test.csv', header = FALSE))
 
 # c. Partition the training set for classification of 0, 1 and 3, 5 classes based on the class 
 # label (last row 785): train_0_1, train_3_5. 
+
 print('Seperating Training sets')
 
 train_0_1 <- train[,train[785,] <=1]
@@ -21,6 +22,7 @@ train_3_5 <- train[,train[785,] >=3]
 train_3_5 <- train_3_5[1:784,]
 
 # d. Do the same for the test set: test_0_1, test_3_5. 
+
 print('Seperating Test sets')
 
 test_0_1 <- test[,test[785,] <=1]
@@ -29,8 +31,9 @@ test_0_1 <- test_0_1[1:784,]
 test_3_5 <- test[,test[785,] >=3]
 test_3_5 <- test_3_5[1:784,]
 
-# e. Separate the class label from all the partitions created (remove row 785 from the 
-#                                                              actual data and store it as a separate vector). 
+# e. Separate the class label from all the partitions created
+# (remove row 785 from the actual data and store it as a separate vector). 
+
 print('Seperating Training labels and relabelling to -1/1')
 true_label_train_0_1 <- train[785, train[785,] <=1]
 true_label_train_0_1[true_label_train_0_1 == 0] <- -1
@@ -48,14 +51,15 @@ true_label_test_3_5[true_label_test_3_5 == 3] <- -1
 true_label_test_3_5[true_label_test_3_5 == 5] <- 1
 
 # f. You will finally have two sets of data and their corresponding true labels: 
-#   - for classification of digits 0, 1: (train_0_1, test_0_1)  and (true_label_train_0_1, true_label_test_0_1);   
-#   - for classification of digits 3, 5: (train_3_5, test_3_5) and (true_label_train_3_5, true_label_test_3_5); 
+# - for classification of digits 0, 1: (train_0_1, test_0_1)  and (true_label_train_0_1, true_label_test_0_1);   
+# - for classification of digits 3, 5: (train_3_5, test_3_5) and (true_label_train_3_5, true_label_test_3_5); 
 
 # TRUE
 
 # g. Visualize 1 image from each class to ensure you have read in the data correctly. You 
 # will have 4 images corresponding to 0, 1, 3 and 5. You need to convert the 1D image 
 # data into 2D for visualisation. 
+
 print('Printing image samples from each set')
 
 show_image <- function(row) {
@@ -80,41 +84,27 @@ par(mfrow=c(1,1))
 
 # 2. Implementation
 
-compute.theta <- function(x, y, theta, alpha, vectorized=FALSE, stocastic=TRUE) {
-  if (stocastic == TRUE) {
-    # Stocastic gradient decent
-    i <- sample(1:ncol(x), 1)
-    yx = y[i] * x[,i]
-    h = t(theta) %*% x[,i]
-    gradient <- yx / (1 + exp(-y[i] * h))
-    sample_size = 1
-  } else if (vectorized) {
-    # Vectorized batch gradient decent
-    yx = y * x
-    h = t(x) %*% theta
-    error = -y * h
-    gradient <- rowSums(sweep(yx, 2, (1 + exp(error)), `/`), 1)
-    sample_size = ncol(x)
-  } else {
-    # Semi-vectorized batch gradient decent
-    gradient <- as.vector(rep(0, nrow(x)))
-    for (i in 1:ncol(x)) {
-      yx = y[i] * x[,i]
-      h = t(theta) %*% x[,i]
-      gradient <- gradient + yx / (1 + exp(-y[i] * h))
-    }
-    sample_size = ncol(x)
-  }
-  return(theta - (1 / sample_size) * alpha * gradient)
+compute.theta <- function(x, y, theta, alpha) {
+  # Stocastic gradient decent
+  i <- sample(1:ncol(x), 1)
+  yx = y[i] * x[,i]
+  h = t(theta) %*% x[,i]
+  gradient <- yx / (1 + exp(-y[i] * h))
+  return(theta - alpha * gradient)
 }
 
-logistic.regression <- function(x, y, alpha=0.1, delta_alpha=0.9, epsilon=1e-4, vectorized=FALSE, stocastic=TRUE, adaptive_alpha=TRUE, max_time=60, max_iterations=10000) {
-  print(paste('Starting logistic regression on ', ncol(x), ' samples with ', nrow(x), ' features'))
+logistic.regression <- function(x, y, alpha=0.03, epsilon=1e-3, max_iterations=10000,
+                                theta=NULL, alternate_stop=FALSE, verbose=FALSE, plot=FALSE) {
+  
+  print(paste('Starting logistic regression on', ncol(x), 'samples with', nrow(x), 'features'))
+  
   # Add feature for bias term
   x <- rbind(x, rep(1, ncol(x)))
   
   # Initialize theta with random values between 0 and 1
-  theta <- as.matrix(runif(nrow(x)))
+  if (is.null(theta)) {
+    theta <- as.matrix(runif(nrow(x)))
+  }
   theta_old <- as.matrix(rep(0, nrow(x)))
   
   # Start timer and iterations
@@ -124,15 +114,14 @@ logistic.regression <- function(x, y, alpha=0.1, delta_alpha=0.9, epsilon=1e-4, 
   cost.log <- as.vector(cost(x, y, theta))
   delta_cost <- 1
   
-  # Continue training iterations until the l1-norm of delta_theta is below the epsilon or max_time has elapsed
-  # Also continue if gradiant is zero to prevent premature exit
-  while(delta_cost > epsilon && iterations < max_iterations) {
+  # Continue training iterations until the delta_cost is below the epsilon or max_iterations has been reached
+  while((delta_cost > epsilon && iterations < max_iterations) || alternate_stop) {
     iterations = iterations + 1
     # Print periodic updates
-    if (as.numeric(Sys.time() - last.time) > 3) {
-      print(paste('Training iterations: ', iterations))
-      print(paste('Delta cost: ', delta_cost))
-      print(paste('Training accuracy: ', accuracy(theta, x, y)))
+    if (verbose && as.numeric(Sys.time() - last.time) > 3) {
+      print(paste('Training iterations:', iterations))
+      print(paste('Delta cost:', delta_cost))
+      print(paste('Training accuracy:', accuracy(theta, x, y)))
       last.time <- Sys.time()
     }
     
@@ -143,21 +132,35 @@ logistic.regression <- function(x, y, alpha=0.1, delta_alpha=0.9, epsilon=1e-4, 
     if (iterations %% 100 == 0) {
       cost.log <- c(cost.log, cost(x, y, theta))
       delta_cost <- abs(diff(tail(cost.log, 2))) / tail(cost.log, 2)[1]
-      flush.console()
-      plot(cost.log, type='l', main="Learning curve", xlab="iterations (00s)", ylab="cost")
+      
+      if (plot) {
+        flush.console()
+        plot(cost.log, type='l', main="Learning curve", xlab="iterations (00s)", ylab="cost")
+      }
     }
+    
+    # Alternative stopping criteria
+    if (alternate_stop) {
+      delta_theta <- theta - theta_old
+      if (norm(delta_theta) < epsilon) {
+        break
+      }
+      theta_old <- theta
+    }
+    
   }
   
-  flush.console()
-  plot(cost.log, type='l', main="Learning curve", xlab="iterations (00s)", ylab="cost")
+  if (plot) {
+    flush.console()
+    plot(cost.log, type='l', main="Learning curve", xlab="iterations (00s)", ylab="cost")
+  }
   
-  print(paste('Training iterations: ', iterations))
-  print(paste('Delta cost: ', delta_cost))
-  print(paste('Elapsed time: ', round(as.numeric(Sys.time() - start.time)), ' sec'))
-  print(paste('Training accuracy: ', accuracy(theta, x, y)))
+  print(paste('Training iterations:', iterations))
+  print(paste('Delta cost:', delta_cost))
+  print(paste('Elapsed time:', round(as.numeric(Sys.time() - start.time)), ' sec'))
+  print(paste('Training accuracy:', accuracy(theta, x, y)))
   return(theta)
 }
-
 
 # Cost Function... negative log likelyhood
 
@@ -194,57 +197,100 @@ accuracy <- function(theta, x, y) {
 
 # 3. Training
 
-alpha=0.03
-epsilon=3e-3
-stocastic=TRUE
-max_iterations=1e5
-
 # a. Train 2 models, one on the train_0_1 set and another on train_3_5, and report the training and test accuracies. 
-theta_0_1 <- logistic.regression(train_0_1, true_label_train_0_1, alpha=alpha, epsilon=epsilon, stocastic=stocastic, max_iterations=max_iterations)
-theta_3_5 <- logistic.regression(train_3_5, true_label_train_3_5, alpha=alpha, epsilon=epsilon, stocastic=stocastic, max_iterations=max_iterations)
 
-print(paste('train_0_1 accuracy: ', accuracy(theta_0_1, train_0_1, true_label_train_0_1)))
-print(paste('test_0_1 accuracy: ', accuracy(theta_0_1, test_0_1, true_label_test_0_1)))
+theta_0_1 <- logistic.regression(train_0_1, true_label_train_0_1)
+theta_3_5 <- logistic.regression(train_3_5, true_label_train_3_5)
 
-print(paste('train_3_5 accuracy: ', accuracy(theta_3_5, train_3_5, true_label_train_3_5)))
-print(paste('test_3_5 accuracy: ', accuracy(theta_3_5, test_3_5, true_label_test_3_5)))
+print(paste('train_0_1 accuracy:', accuracy(theta_0_1, train_0_1, true_label_train_0_1)))
+print(paste('test_0_1 accuracy:', accuracy(theta_0_1, test_0_1, true_label_test_0_1)))
 
-# b. Repeat 3a 10 times, i.e. you should obtain 10 train and test accuracies for each set. Calculate the average train and test accuracies over the 10 runs, and report them. 
+print(paste('train_3_5 accuracy:', accuracy(theta_3_5, train_3_5, true_label_train_3_5)))
+print(paste('test_3_5 accuracy:', accuracy(theta_3_5, test_3_5, true_label_test_3_5)))
 
-accuracy_train_0_1 <- vector()
-accuracy_train_3_5 <- vector()
-accuracy_test_0_1 <- vector()
-accuracy_test_3_5 <- vector()
+# b. Repeat 3a 10 times, i.e. you should obtain 10 train and test accuracies for each set.
+# Calculate the average train and test accuracies over the 10 runs, and report them. 
 
-for (i in 1:10) {
-  theta_0_1 <- logistic.regression(train_0_1, true_label_train_0_1, alpha=alpha, epsilon=epsilon, stocastic=stocastic, max_iterations=max_iterations)
-  theta_3_5 <- logistic.regression(train_3_5, true_label_train_3_5, alpha=alpha, epsilon=epsilon, stocastic=stocastic, max_iterations=max_iterations)
+avg_reg <- function(x, y, test_x, test_y, theta=NULL, epsilon=1e-3, alternate_stop=FALSE) {
+  accuracy_train <- vector()
+  accuracy_test <- vector()
   
-  accuracy_train_0_1 <- c(accuracy_train_0_1 , accuracy(theta_0_1, train_0_1, true_label_train_0_1))
-  accuracy_test_0_1 <- c(accuracy_test_0_1 , accuracy(theta_0_1, test_0_1, true_label_test_0_1))
+  for (i in 1:10) {
+    print(paste('Running experiment', i))
+    theta <- logistic.regression(x, y, theta=theta, epsilon=epsilon, alternate_stop=alternate_stop)
+    accuracy_train <- c(accuracy_train, accuracy(theta, x, y))
+    accuracy_test <- c(accuracy_test, accuracy(theta, test_x, test_y))
+  }
   
-  accuracy_train_3_5 <- c(accuracy_train_3_5 , accuracy(theta_3_5, train_3_5, true_label_train_3_5))
-  accuracy_test_3_5 <- c(accuracy_test_3_5 , accuracy(theta_3_5, test_3_5, true_label_test_3_5))
+  return(c(mean(accuracy_train), mean(accuracy_test)))
 }
+  
+accuracy_0_1 <- avg_reg(train_0_1, true_label_train_0_1, test_0_1, true_label_test_0_1)
+accuracy_3_5 <- avg_reg(train_3_5, true_label_train_3_5, test_3_5, true_label_test_3_5)
 
-print(paste('Average train_0_1 accuracy: ', mean(accuracy_train_0_1)))
-print(paste('Average test_0_1 accuracy: ', mean(accuracy_test_0_1)))
-print(paste('Average train_3_5 accuracy: ', mean(accuracy_train_3_5)))
-print(paste('Average test_3_5 accuracy: ', mean(accuracy_test_3_5)))
+print(paste('Average train_0_1 accuracy:', mean(accuracy_0_1[1])))
+print(paste('Average test_0_1 accuracy:', mean(accuracy_0_1[2])))
 
-# c. For 0,1 and 3,5 cases, explain if you observe any difference you in accuracy. Also, explain why do you think this difference might be. 
+print(paste('Average train_3_5 accuracy:', mean(accuracy_3_5[1])))
+print(paste('Average test_3_5 accuracy:', mean(accuracy_3_5[2])))
 
-# d. This assignment deals with binary classification. Explain what you would do if you had more than two classes to classify, using logistic regression. 
 
+# c. For 0,1 and 3,5 cases, explain if you observe any difference you in accuracy.
+# Also, explain why do you think this difference might be. 
+
+# d. This assignment deals with binary classification.
+# Explain what you would do if you had more than two classes to classify, using logistic regression. 
+
+
+# 4. Evaluation
+# 3/5 ONLY
+
+# a. Experiment with different initializations of the parameter used for gradient descent.
+# Clearly mention the initial values of the parameter tried, run the same experiment 
+# as 3b using this initialization, report the average test and train accuracies obtained 
+# by using this initialization, mention which is set of initializations is the better. 
+
+# NOTES FROM PIAZZA
+# Ananya Raval 
+# 1. Q4a: A , different initialisations of weight parameters ONLY.
+# 2. Q4b: convergence criteria refers to how to choose to stop your gradient descent. One way is number of iterations as you mentioned. then choose another way to stop gradient descent.
+# 3. yes, total 2 X 10 runs. you only need to compare (EXPERIMENT vs BASELINE), see which is better and write a few comments of why you think that is.
+
+theta <- as.matrix(rep(1e-9, nrow(train_3_5) + 1))
+accuracy_alternate_theta <- avg_reg(train_3_5, true_label_train_3_5, test_3_5, true_label_test_3_5, theta=theta)
+print(paste('Average alternate theta train_3_5 accuracy:', mean(accuracy_alternate_theta[1])))
+print(paste('Average alternate theta test_3_5 accuracy:', mean(accuracy_alternate_theta[2])))
+
+# b. Experiment with different convergence criteria for gradient descent. Clearly mention 
+# the new criteria tried, run the same experiment as 3b using this new criteria, report 
+# average test and train accuracies obtained using this criteria, mention which set of 
+# criteria is better. 
+
+accuracy_alternate_stop <- avg_reg(train_3_5, true_label_train_3_5, test_3_5, true_label_test_3_5, epsilon=1e-12, alternate_stop=TRUE)
+print(paste('Average alternate stop train_3_5 accuracy:', mean(accuracy_alternate_stop[1])))
+print(paste('Average alternate stop test_3_5 accuracy:', mean(accuracy_alternate_stop[2])))
+
+
+# 5. Learning Curves
+
+# a. For each set of classes (0,1 and 3,5), choose the following sizes to train on: 5%, 10%, 
+# 15% ... 100% (i.e. 20 training set sizes). For each training set size, sample that many 
+# inputs from the respective complete training set (i.e. train_0_1 or train_3_5). Train 
+# your model on each subset selected, test it on the corresponding test set (i.e. 
+# test_0_1 or test_3_5), and graph the training and test set accuracy over each split 
+# (you should end up with TWO graphs - one showing training & test accuracy for 0,1 
+# and another for 3,5 set). Remember to average the accuracy over 10 different 
+# divisions of the data each of the above sizes so the graphs will be less noisy. 
+# Comment on the trends of accuracy values you observe for each set. 
+
+# b. Repeat 5a, but instead of plotting accuracies, plot the logistic loss/negative log 
+# likelihood when training and testing, for each size. Comment on the trends of loss 
+# values you observe for each set. 
+
+
+# MISC
 s_x = rbind(train_3_5[,6122:6142], rep(1, ncol(s_x)))
 s_y = true_label_train_3_5[6122:6142]
 h <- 1 / (1 + exp(t(s_x) %*% theta_3_5))
 p = classify(theta_3_5, s_x)
 test_fit(p, s_y)
-
-
-
-
-# 4. Evaluation
-
-# 5. Learning Curves
